ca6ffe7bdc1d463c66d86ca5d122fc6087f57a4e
Andrew Gordon <Agordon03@bellarmine.edu>
2022-04-07 19:30:12 -0400

    first commit

---
+++ js/madlib.js
@@ -0,0 +1,15 @@
+function changeColor(){
+    const tboxes= document.getElementById("name");
+    const tboxes2=document.getElementById("adjective");
+    const tboxes3=document.getElementById("noun");
+
+
+    tboxes.style.color="red";
+    tboxes2.style.color="red";
+    tboxes3.style.color="red";
+
+}
+
+function generateMadlib(){
+    
+}
