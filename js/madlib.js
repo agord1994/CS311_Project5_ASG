15f588508d380d0c366e99fe22eac8b9ce55cf49
Andrew Gordon <Agordon03@bellarmine.edu>
2022-04-09 13:17:09 -0400

    last draft

--- js/madlib.js
+++ js/madlib.js
@@ -15,7 +15,7 @@
     let text = document.getElementsByClassName("tboxes");
     let  container=document.getElementById("words");
     let madlibbox=document.createElement("div");
-    var counter=0;
+    let amountOFClicks=0;
  
   
 
@@ -38,17 +38,25 @@ function hideContent(){
 
 function showContent(){
    
-   
+    
     madlibbox.style.display= "none";
+    container.style.justifyContent="center";
+    container.style.display="flex";
+    container.style.flexDirection="column";
+    container.style.paddingLeft="22% ";
+    container.style.paddingRight="22%";
+  
 
     for(var i=0; i<text.length;i++){
         myB.style.display="block";
     text[i].style.display="block";
+    
+   
      }
 
      for (var i=0;i<hsix.length;i++){ 
         hsix[i].style.display = "block";
-
+       
        }
    
    
@@ -64,7 +72,9 @@ function generateMadlib(){
     container.appendChild(madlibbox);
     madlibbox.classList.add("madlib");
     madlibbox.setAttribute('id',"madlib1");
-
+    madlibbox.style.display="flex";
+    madlibbox.style.flexDirection="column";
+  
 
     let text1=document.createTextNode("A vacation is when you take a trip to some "+tboxes.value+" place with your "+ tboxes2.value+" family. Usually you go to some place that is near a/an "+tboxes3.value+" or up on a/an "+tboxes4.value+". A good vacation place is one where you can ride "+tboxes5.value+" or play "+tboxes6.value+" or go hunting for "+tboxes7.value+". I like to spend my time "+tboxes8.value+" or "+tboxes9.value+". When parents go on vacation, they spend their time eating three "+tboxes10.value+" a day, and fathers play golf,and mothers sit around"+tboxes11.value+".\n");
 
@@ -73,27 +83,30 @@ function generateMadlib(){
     let resetbutton=document.createElement("button");
     resetbutton.setAttribute("id","button-reset");
     resetbutton.textContent= "Reset";
+    resetbutton.style.marginTop="10%";
 
     resetbutton.style.backgroundColor="black";
     resetbutton.style.color= "blueviolet";
     resetbutton.style.padding="5% 5%";
+    
+
+ 
+
     madlibbox.appendChild(resetbutton);
-  
+      
+
      let myResetButton=document.getElementById("button-reset");
   
      myResetButton.addEventListener("click",showContent);
 
-    
-
   
 }
 
+amountOFClicks++;
 
-
-counter++;
+if (amountOFClicks== 1){
 
 myB.addEventListener("click", hideContent);
 myB.addEventListener("click",generateMadlib );
-
-
+}
 
